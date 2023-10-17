2bf6f36be0b6423b9c945a7145f6c817f2a6a204
wailabz <wailaz.maal@gmail.com>
2023-10-17 06:47:24 -0700

    exercice6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar('0' + num);
+};
+putchar('\n');
+return (0);
+}
