a2b1d62380b2b5874ab48bad23e0c0e735835590
wailabz <wailaz.maal@gmail.com>
2023-10-18 05:58:13 -0700

    exercice1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+}
