714cda0952ad6dc99802c1287d638c468c2ea5fb
wailabz <wailaz.maal@gmail.com>
2023-10-22 07:56:36 -0700

    exercice3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
  * print_numbers - print number
  * Description: print numbers
  * Return: void
 */
-
 void print_numbers(void)
 {
 int num;
-
 for (num = 0; num < 10; num++)
 {
-_putchar('0' + num);
+_putchar ('0' + num);
 }
-_putchar('\n');
+_putchar ('\n');
 }
