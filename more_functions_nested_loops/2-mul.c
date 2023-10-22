6b79e0ff2f498a9b258034d18a48951eadca7f7f
wailabz <wailaz.maal@gmail.com>
2023-10-22 07:04:58 -0700

    exercice2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * mul - Entry point
+  * @a: firt int to return
+  * @b: second int to return
+  * Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+int produit = a * b;
+return (produit);
+}
