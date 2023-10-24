b12782a5c3c841376021267d38bc516fefcb771a
wailabz <wailaz.maal@gmail.com>
2023-10-24 02:23:12 -0700

    exercice2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - entry point
+ *
+ * @s: char variable
+ * Description: function that returns the length of a string.
+ * Return: a if success
+*/
+
+int _strlen(char *s)
+{
+int a;
+
+for (a = 0; (s[a] != '\0'); a++)
+{
+}
+return (a);
+}
