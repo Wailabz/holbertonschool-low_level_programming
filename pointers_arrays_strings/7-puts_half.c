836af6c1b2dd7f975fe4f7d6ee419eef738799fb
wailabz <wailaz.maal@gmail.com>
2023-10-24 05:33:53 -0700

    exercice7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ *
+ * @str: function parameter
+ * Description: function that prints half of a string
+ * Return: nothing
+*/
+
+void puts_half(char *str)
+{
+int letter;
+
+for (letter = 0; str[letter] != '\0'; letter++)
+
+
+for (letter /= 2; str[letter] != '\0'; letter++)
+{
+_putchar(str[letter]);
+}
+_putchar('\n');
+}
