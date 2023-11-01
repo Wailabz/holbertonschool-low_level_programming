21924da4ef85cb3a6a06458f08bfd01aa0f76976
wailabz <wailaz.maal@gmail.com>
2023-11-01 11:04:04 -0700

    exercice0

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,3 +1,15 @@
 #include "main.h"
-#include <sdtio.h>
-
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be printed
+ *
+ * Return : 0
+ */
+void _puts_recursion(char *s);
+int string_length(char *s)
+{
+int c = 0;
+while(*s[c] != '\0')
+c++;
+return c;
+}
