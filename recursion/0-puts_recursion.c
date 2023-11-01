53b849c8f1f8624addb2c84e6c3fdf639a23d172
wailabz <wailaz.maal@gmail.com>
2023-11-01 10:46:14 -0700

    exerccice0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * recursion - entry point
- * description: function that prints a string, followed by a new line.
- *
- * return : Always 0.
- */
+
 void _puts_recursion(char *s);
 {
-if (*s !=0)
+if (*s != 0)
 {
 _putchar(*s);
-_puts_recursion(s +1);
+_puts_recursion(s + 1);
 }
 else
 {
