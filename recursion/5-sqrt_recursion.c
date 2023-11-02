1c78109df4397f9791abcbd0b75e872e9000a6e2
wailabz <wailaz.maal@gmail.com>
2023-11-02 12:07:34 -0700

    exercice5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -14,6 +14,11 @@ if ((x * x) > n)
 return (-1);
 return (_sqrt_recursion_runtime(n, x + 1));
 }
+/**
+ * _sqrt_recursion - function
+ * @n: int*
+ * Return: int
+ */
 int _sqrt_recursion(int n)
 {
 return (_sqrt_recursion_runtime(n, 0));
