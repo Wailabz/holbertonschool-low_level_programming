567cadcd68786f2392dfe4d3b2cd02d3a97c3f39
wailabz <wailaz.maal@gmail.com>
2023-12-22 12:08:56 -0800

    ex3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,32 +1,34 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - entry point
+ * @s: pointer intial segment string
+ * @accept: new segment string
+ * Description: function that gets the length of a prefix substring.
+ * Return: Returns the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+break;
+}
+if (accept[j] == '\0')
+return (i);
+}
+return (i);
+}
 
 /**
- * *_strncpy - entry point
- * @dest: pointer
- * @src: pointer
- * @n: var
- * Description: function that copies a string.
- * Return: string if success
+ * Starting by going through the original string @s, we will stop when '\0'
+ * We're going to do the same to the @accept string, we will stop when '\0'
+ * We're comparing the letter in @s with all the letters in @accept
+ * If it match we stop looking through the rest of letters in @accept
+ * If accept[j] = '\0' it means that there is no match between @s and @accept
+ * So we send back the number the were matching before this iteration 
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-char *tmp1 = dest;
-char *tmp2 = src;
-
-while (src < tmp2 + n && *src != '\0')
-{
-*dest = *src;
-src++;
-dest++;
-}
-while (src < tmp2 + n)
-{
-*dest = '\0';
-dest++;
-src++;
-}
-dest = tmp1;
-src = tmp2;
-
-return (dest);
-}
