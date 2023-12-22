5cc942b0c059543a5c06f3cd91fc98669fe497ea
wailabz <wailaz.maal@gmail.com>
2023-12-22 04:00:51 -0800

    ex

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: function that print the filename
+ * Return: 0 if success
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
