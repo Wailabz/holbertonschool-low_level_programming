821ec367fc1a7d750bf91d342009ad718c5f5fd0
wailabz <wailaz.maal@gmail.com>
2024-10-27 19:52:31 +0000

    ex

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: always 0 (success)
+  */
+
+/* betty style doc for function main goes there */
+
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+
+return (0);
+}
