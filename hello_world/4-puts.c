d0bd13e4e4a6efd2c1dc85e955615400e43e1959
wailabz <wailaz.maal@gmail.com>
2024-10-27 18:59:39 +0000

    ex4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Script used to print a message without use printf
+ *
+ * Return: 0 = success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
