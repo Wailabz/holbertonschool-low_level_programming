05cd078834ce82db7c033bcb7058da9407c9c42a
wailabz <wailaz.maal@gmail.com>
2024-11-03 20:05:53 +0000

    ex

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * reset_to_98 - Entry point
+  * Description : take a pointer
+  * @n: parameter of my function
+  */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+
+}
