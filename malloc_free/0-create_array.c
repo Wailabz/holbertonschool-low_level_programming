e3928a498814fda863962caaf27e8731c665fb69
Wailabz <wailaz.maal@gmail.com>
2024-11-15 16:06:49 +0100

    ex

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return NULL if size = 0
- * 
- * Return: Nothing.
+ * create_array - entry point
+ * @size: is the the number of character @c in our array
+ * @c: character that will fill our array
+ * Description: function that creates an array of chars
+ * Return: Null if size is 0, return pointer if success
  */
 
-char *create_array(unsigned int size, char c){
-    char tableau[] = {};
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *tableau;
 
-    for (int i = size; i > 0; i--)
-    {
-        tableau[i] = c;
-    }
-
-    char *tableau[] = tableau;
-
-    if(size == 0){
-        return NULL;
-    } else {
-        return *tableau; 
-    }
+	if (size == 0)
+		return (NULL);
+	tableau = malloc(sizeof(char) * size);
+	if (tableau == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		tableau[i] = c;
+	return (tableau);
 }
